d742419b3132eea3c0b292a88ef76887555c0bc1
Wali <wali.rahman89@yahoo.com>
2019-08-24 16:31:47 -0400

    psuedo code added

--- async-example.js
+++ async-example.js
@@ -5,3 +5,15 @@ const async   = require('async');
 const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
 const WEATHER_API_KEY = '88bfde3533d98e8a300a138ef668cda2';
 
+async.waterfall([
+    (callback) => {
+      console.log("Here we'll ask for the user's input.");
+      callback();
+    },
+    (callback) => {
+      console.log("Here we'll request the weather data.");
+      callback();
+    }
+  ], (err, results) => {
+    console.log("Here we'll output the result.");
+  });
