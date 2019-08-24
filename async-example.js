107912977e4fa34968aa7eadcd4873900a720e9c
Wali <wali.rahman89@yahoo.com>
2019-08-24 16:44:57 -0400

    solution added

--- async-example.js
+++ async-example.js
@@ -7,13 +7,23 @@ const WEATHER_API_KEY = '88bfde3533d98e8a300a138ef668cda2';
 
 async.waterfall([
     (callback) => {
-      console.log("Here we'll ask for the user's input.");
-      callback();
+      prompt.get({
+        name: 'city',
+        description: 'Enter city to fetch its current weather'
+      }, (err, result) => {
+        if (err) return callback(err);
+        callback(null, result.city);
+      });
     },
-    (callback) => {
-      console.log("Here we'll request the weather data.");
-      callback();
+    (city, callback) => {
+      const url = WEATHER_API_URL + city + '&APPID=' + WEATHER_API_KEY +
+          '&units=imperial';
+      request(url, (err, resp, body) => {
+        if (err) return callback(err);
+        callback(null, city, body);
+      });
     }
-  ], (err, results) => {
-    console.log("Here we'll output the result.");
+  ], (err, city, weather) => {
+    if (err) console.error(err);
+    console.log(`The weather in ${city} \n ${weather}`);
   });
